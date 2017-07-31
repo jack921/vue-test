61614d57d29d5e745f305fea085bdbdff69c6ef9
jack921 <355596943@qq.com>
2017-07-31 21:12:46 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <AppBar class="header"></AppBar>
+    <router-view class="content"></router-view>
+    <BottomNavigation></BottomNavigation>
   </div>
 </template>
 
 <script>
+import AppBar from './components/Header.vue'
+import BottomNavigation from './components/BottomNavigation.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    AppBar,
+    BottomNavigation
+  }
 }
 </script>
 
@@ -18,6 +26,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index:2;
+}
+.content{
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <mu-appbar title="DouBan">
+    <mu-icon-button icon="menu" slot="left"/>
+    <mu-flat-button label="更多" slot="right"/>
+    <mu-icon-button icon="expand_more" slot="right"/>
+  </mu-appbar>
+</template>
