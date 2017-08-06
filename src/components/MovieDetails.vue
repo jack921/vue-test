0621180175011b5e59269f510791a250e90e6db3
jack921 <355596943@qq.com>
2017-08-07 01:07:47 +0800

    init

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
-        <h1>Hello Jack</h1>
+        <p style="font-size: 35px;text-align: left;margin-top: 80px;margin-left: 30px">Hello Jack</p>
+        <div style="margin-top: -20px">
+            <div style="width:60%;float: left">
+                <div style="display: flex">
+                  <p style="margin-left: 30px;font-size: 20px">pingfen</p>
+                  <p style="margin-left: 20px;font-size: 20px">217254pingjia</p>
+                </div>
+                <p style="text-align: left;font-size: 24px;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;margin-top: -3px">
+                  机器人9号（伊利亚•伍德 Elijah Wood 饰）突然醒来，发现身边的世界充满危机，四处残败，
+                  一片末世景象。9号带着一个画有三个奇怪符号的圆形物体逃到街上，幸遇发明家机器人2号（马丁•兰道 Martin Landau 饰）
+                  给自己装上了声音，但2号却不幸被机器怪兽抓走。9号找到了老兵1号（克里斯托弗•普卢默 Christopher Plummer 饰）、
+                  机械工5号（约翰•雷利 John C. Reilly 饰）、疯癫画家6号（克里斯品•格拉夫 Crispin Glover 饰）和大力士8号（弗雷德•塔塔绍尔 Fred Tatasciore 饰）。
+                  9号与5号擅自出行援救2号，危急时被女武士7号（詹妮佛•康纳利 Jennifer Connelly 饰）救下，但无意中9号却令终极机器兽复活。
+                  带着自己从哪里来以及生存使命的问题，9号决定想尽办法制服机器兽，拯救全世界……©豆瓣</p>
+
+            </div>
+            <div style="width: 40%;float: left">
+                  <img src="https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p494268647.webp" style="width: 340px;height:500px"/>
+            </div>
+        </div>
+
+        <p style="font-size: 20px;text-align: left;margin-top: 80px;margin-left: 30px">HelloJackjianjie</p>
+
+        <p style="text-align: left;font-size: 20px;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;margin-top: -3px">
+          机器人9号（伊利亚•伍德 Elijah Wood 饰）突然醒来，发现身边的世界充满危机，四处残败，
+          一片末世景象。9号带着一个画有三个奇怪符号的圆形物体逃到街上，幸遇发明家机器人2号（马丁•兰道 Martin Landau 饰）
+          给自己装上了声音，但2号却不幸被机器怪兽抓走。9号找到了老兵1号（克里斯托弗•普卢默 Christopher Plummer 饰）、
+          机械工5号（约翰•雷利 John C. Reilly 饰）、疯癫画家6号（克里斯品•格拉夫 Crispin Glover 饰）和大力士8号（弗雷德•塔塔绍尔 Fred Tatasciore 饰）。
+          9号与5号擅自出行援救2号，危急时被女武士7号（詹妮佛•康纳利 Jennifer Connelly 饰）救下，但无意中9号却令终极机器兽复活。
+          带着自己从哪里来以及生存使命的问题，9号决定想尽办法制服机器兽，拯救全世界……©豆瓣</p>
 
     </div>
 </template>
