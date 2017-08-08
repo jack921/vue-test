06f75182534c7aa329465a6a4667e9c94706493e
jack921 <355596943@qq.com>
2017-08-08 20:58:12 +0800

    init

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -13,24 +13,24 @@
                   <p style="margin-left: 30px;font-size: 20px">pingfen</p>
                   <p style="margin-left: 20px;font-size: 20px">217254pingjia</p>
                 </div>
-              <mu-card style="text-align: left;font-size: 24px;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;margin-top: -3px">
-                <p style="padding: 10px">
-                  机器人9号（伊利亚•伍德 Elijah Wood 饰）突然醒来，发现身边的世界充满危机，四处残败，
-                  一片末世景象。9号带着一个画有三个奇怪符号的圆形物体逃到街上，幸遇发明家机器人2号（马丁•兰道 Martin Landau 饰）
-                  给自己装上了声音，但2号却不幸被机器怪兽抓走。9号找到了老兵1号（克里斯托弗•普卢默 Christopher Plummer 饰）、
-                  机械工5号（约翰•雷利 John C. Reilly 饰）、疯癫画家6号（克里斯品•格拉夫 Crispin Glover 饰）和大力士8号（弗雷德•塔塔绍尔 Fred Tatasciore 饰）。
-                  9号与5号擅自出行援救2号，危急时被女武士7号（詹妮佛•康纳利 Jennifer Connelly 饰）救下，但无意中9号却令终极机器兽复活。
-                  带着自己从哪里来以及生存使命的问题，9号决定想尽办法制服机器兽，拯救全世界……©豆瓣
-
-                </p>
+              <mu-card style="text-align: left;font-size: 18px;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;margin-top: -3px">
+                 <p style="margin-top: 30px;margin-left: 10px">类型</p>
+                 <p style="margin-top: 30px;margin-left: 10px">年代</p>
+                 <p style="margin-top: 30px;margin-left: 10px">地区</p>
+                 <p style="margin-top: 30px;margin-left: 10px">又名</p>
+                 <p style="margin-top: 30px;margin-left: 10px">想看人数</p>
+                 <p style="margin-top: 30px;margin-left: 10px">看过人数</p>
+                 <p style="margin-top: 30px;margin-left: 10px">评分人数</p>
+                 <p style="margin-top: 30px;margin-left: 10px">短评数量</p>
+                 <p style="margin-top: 30px;margin-left: 10px">影评数量</p>
               </mu-card>
             </div>
-            <div style="width: 40%;float: left;margin-top: -70px">
+            <div style="width: 40%;display: flex;margin-top: -70px">
                   <img src="https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p494268647.webp" style="width: 340px;height:500px"/>
             </div>
         </div>
 
-        <p style="font-size: 20px;text-align: left;margin-top: 80px;margin-left: 30px">HelloJackjianjie</p>
+      <p style="font-size: 20px;text-align: left;margin-top: 80px;margin-left: 30px;display: flex">HelloJackjianjie</p>
 
       <mu-card style="text-align: left;font-size: 20px;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;margin-top: -3px">
         <p style="padding: 10px;margin-bottom: 30px">
